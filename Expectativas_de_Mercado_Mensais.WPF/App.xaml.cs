f2efc4b95cf6cfe07538d97a0cf9e36870c3c7fb
Ribeiro <alexfariakof@gmail.com>
2024-02-02 22:35:18 -0300

    Scafold WPF UI

---
+++ Expectativas_de_Mercado_Mensais.WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Expectativas_de_Mercado_Mensais.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
