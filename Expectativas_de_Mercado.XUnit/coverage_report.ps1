0a84931f8e9cc5483985e89ca7084bd8ae98bb7f
Ribeiro <alexfariakof@gmail.com>
2024-02-08 01:50:42 -0300

    Novas Implementações de Testes Unitários

--- Expectativas_de_Mercado.XUnit/coverage_report.ps1
+++ Expectativas_de_Mercado.XUnit/coverage_report.ps1
@@ -5,5 +5,5 @@ $reportPath = Join-Path -Path $projectTestPath -ChildPath "TestResults"
 $coverageXmlPath = Join-Path -Path (Join-Path -Path $projectTestPath -ChildPath "TestResults") -ChildPath "coveragereport"
 
 # Excuta Teste Unitarios sem restore e build e gera o relatório de cobertura do Backend
-dotnet test ./Expectativas_de_Mercado.XUnit.csproj --results-directory $reportPath /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura --collect:"XPlat Code Coverage;Format=opencover" --no-restore --no-build > $null 2>&1
+dotnet test ./Expectativas_de_Mercado.XUnit.csproj --results-directory $reportPath /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura --collect:"XPlat Code Coverage;Format=opencover" --no-restore --no-build
 reportgenerator -reports:$projectTestPath\coverage.cobertura.xml -targetdir:$coverageXmlPath -reporttypes:"Html;lcov;" -sourcedirs:$sourceDirs
