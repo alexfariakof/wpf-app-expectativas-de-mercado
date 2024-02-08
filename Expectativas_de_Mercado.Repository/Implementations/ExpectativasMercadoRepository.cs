91924acd02302bf3a7d06ef6522c397581a196a1
Ribeiro <alexfariakof@gmail.com>
2024-02-08 09:02:57 -0300

    Implementação Repositório de dados

---
+++ Expectativas_de_Mercado.Repository/Implementations/ExpectativasMercadoRepository.cs
@@ -0,0 +1,55 @@
+﻿using Expectativas_de_Mercado.Model.Aggregates;
+using Expectativas_de_Mercado.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Repository;
+using System.Data;
+
+namespace Expectativas_de_Mercado.Repository.Implementations;
+public class ExpectativasMercadoRepository : IExpectativasMercadoRepository
+{
+    private readonly RegisterContext _context;
+    private DbSet<ExpectativasMercado> dataSet;
+    public ExpectativasMercadoRepository(RegisterContext context)
+    {
+        _context = context;
+        dataSet = context.Set<ExpectativasMercado>();
+    }
+    public bool Exists(DateTime dtInicial, DateTime dtFinal)
+    {
+        try
+        {
+            var mindate = dataSet.Select(em => em.Data).Min().Value;
+            var maxdate = dataSet.Select(em => em.Data).Max().Value;
+            return (mindate == dtInicial && maxdate == dtFinal);
+        }
+        catch 
+        {
+            return false;
+        }        
+    }
+
+    public List<ExpectativasMercado> Get(DateTime dtInicial, DateTime dtFinal)
+    {
+        try
+        {
+            return dataSet.Where(em => em.Data >= dtInicial && em.Data <= dtFinal).ToList();
+        }
+        catch 
+        {
+            throw new ArgumentException("ExpectativasMercado_Repositorio_Get");
+        }
+    }
+    public ExpectativasMercado Insert(ExpectativasMercado expectativasMercado)
+    {
+        try
+        {
+            dataSet.Add(expectativasMercado);
+            _context.SaveChanges();
+        }
+        catch 
+        {
+            throw new ArgumentException("expectativasMercado_Repositorio_Insert"); ;
+        }
+        return expectativasMercado;
+    }
+}
